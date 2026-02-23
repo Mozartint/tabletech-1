29eaa51cd0a08544639b493aa93daf4d3304b335
Mozartint <dmusti74@gmail.com>
2026-02-23 04:21:45 +0300

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,30 +1,28 @@
-import React from 'react';
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
-import './App.css';
-import Login from './pages/Login';
-import AdminDashboard from './pages/AdminDashboard';
-import OwnerDashboard from './pages/OwnerDashboard';
-import KitchenDashboard from './pages/KitchenDashboard';
-import CashierDashboard from './pages/CashierDashboard';
-import QRMenu from './pages/QRMenu';
-import { Toaster } from './components/ui/sonner';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Login from "./pages/Login";
+import AdminDashboard from "./pages/AdminDashboard";
+import CashierDashboard from "./pages/CashierDashboard";
+import KitchenDashboard from "./pages/KitchenDashboard";
+import OwnerDashboard from "./pages/OwnerDashboard";
+import QRMenu from "./pages/QRMenu";
 
 function App() {
   return (
-    <>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Navigate to="/login" replace />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/admin" element={<AdminDashboard />} />
-          <Route path="/owner" element={<OwnerDashboard />} />
-          <Route path="/kitchen" element={<KitchenDashboard />} />
-          <Route path="/cashier" element={<CashierDashboard />} />
-          <Route path="/menu/:tableId" element={<QRMenu />} />
-        </Routes>
-      </BrowserRouter>
-      <Toaster position="top-center" richColors />
-    </>
+    <BrowserRouter>
+      <Routes>
+
+        {/* QR MENU - EN KRİTİK ROUTE */}
+        <Route path="/menu/:tableId" element={<QRMenu />} />
+
+        {/* Diğer sayfalar */}
+        <Route path="/" element={<Login />} />
+        <Route path="/admin" element={<AdminDashboard />} />
+        <Route path="/cashier" element={<CashierDashboard />} />
+        <Route path="/kitchen" element={<KitchenDashboard />} />
+        <Route path="/owner" element={<OwnerDashboard />} />
+
+      </Routes>
+    </BrowserRouter>
   );
 }
 
